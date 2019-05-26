f80fa0ff06a5f8733b85a5a011e99770e944007d
Jeff Hollan <jeffhollan@outlook.com>
2019-05-26 15:17:23 -0700

    initial

---
+++ GrimReaper/run.ps1
@@ -0,0 +1,16 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Write an information log with the current time.
+Write-Host "The grim reaper arises to find those for whom death tolls ☠️"
+
+$toll = Get-AzResourceGroup | Where-Object ResourceGroupName -like 'deleteme*'
+
+Foreach ($g in $toll)
+{
+    Write-Host "Goodbye $($g.ResourceGroupName) 🔔..."
+    Remove-AzResourceGroup -Name $g.ResourceGroupName -Force
+    Write-Host "⚰️"
+}
+
+Write-Host "🌙 The repear returns to his resting until death tolls again..."
